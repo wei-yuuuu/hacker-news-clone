35a12e85f7b97234f0d4f044530ae2097cf28705
takeu <tony40508@gmail.com>
2019-08-11 01:25:44 +0800

    feat: email regexp

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,11 @@
-export const LINKS_PER_PAGE = 5;
+export const LINKS_PER_PAGE = 5
 
 export function getDomain(url) {
-  return url.replace(/^https?:\/\//i, "");
+  return url.replace(/^https?:\/\//i, '')
 }
 
-/* EMAIL REGEX: !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email) */
+export const EMAIL_REGEX = new RegExp(
+  '^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$',
+  'i'
+)
 /* URL REGEX: !/^(ftp|http|https):\/\/[^ "]+$/.test(values.url) */
