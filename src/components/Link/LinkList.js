c7a3f9197b38ef229d0591b30c396ac172aa0e79
takeu <tony40508@gmail.com>
2019-08-19 19:45:06 +0800

    remove: useAuth

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -2,8 +2,6 @@ import React from 'react'
 import useAuth from '../../hooks/useAuth'
 
 function LinkList(props) {
-  const user = useAuth()
-  console.log({ user })
   return <div>LinkList</div>
 }
 
