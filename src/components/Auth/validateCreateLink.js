b5800398ee89f38b472b1f7dc2612b66cb67e3d7
takeu <tony40508@gmail.com>
2019-08-22 23:26:28 +0800

    feat: CreateLink validaiotn

--- src/components/Auth/validateCreateLink.js
+++ src/components/Auth/validateCreateLink.js
@@ -1,1 +1,18 @@
-export default function validateCreateLink(values) {}
+import { URL_REGEX } from '../../utils'
+
+export default function validateCreateLink(values) {
+  let errors = {}
+
+  if (!values.description) {
+    errors.description = 'Description required'
+  } else if (values.description.length < 10) {
+    errors.description = 'Description must be at least 10 characters'
+  }
+  if (!values.url) {
+    errors.url = 'URL required'
+  } else if (!URL_REGEX.test(values.url)) {
+    errors.password = 'URL must be valid'
+  }
+
+  return errors
+}
--- src/utils/index.js
+++ src/utils/index.js
@@ -8,4 +8,5 @@ export const EMAIL_REGEX = new RegExp(
   '^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$',
   'i'
 )
-/* URL REGEX: !/^(ftp|http|https):\/\/[^ "]+$/.test(values.url) */
+
+export const URL_REGEX = new RegExp('^(ftp|http|https)://[^ "]+$')
