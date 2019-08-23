b8f83e67c3f9b3b9d85e2d0ac98776e786e38382
takeu <tony40508@gmail.com>
2019-08-23 16:26:01 +0800

    wip: fetch links with snapshot

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,7 +1,26 @@
 import React from 'react'
-import useAuth from '../../hooks/useAuth'
+import FirebaseContext from '../../firebase/context'
 
 function LinkList(props) {
+  const { firebase } = React.useContext(FirebaseContext)
+
+  const handleSnapshot = snapshot => {
+    const links = snapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    })
+    console.log({ links })
+  }
+  const getLinks = () => {
+    firebase.db.collection('links').onSnapshot(handleSnapshot)
+  }
+
+  React.useEffect(() => {
+    getLinks()
+  }, [])
+
   return <div>LinkList</div>
 }
 
