89c399ec9082f584e23aa7e852f53ac01ea31bb2
takeu <tony40508@gmail.com>
2019-08-23 16:05:45 +0800

    feat: create links

--- src/components/Link/CreateLink.js
+++ src/components/Link/CreateLink.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 
 import useFormValidation from '../../hooks/useFormValidation'
 import validateCreateLink from '../Auth/validateCreateLink'
+import FirebaseContext from '../../firebase/context'
 
 const INITIAL_STATE = {
   description: '',
@@ -10,13 +11,33 @@ const INITIAL_STATE = {
 }
 
 const CreateLink = props => {
+  const { firebase, user } = React.useContext(FirebaseContext)
   const { handleSubmit, handleChange, values, errors } = useFormValidation(
     INITIAL_STATE,
     validateCreateLink,
     handleCreateLink
   )
+
   function handleCreateLink() {
-    console.log('link')
+    if (!user) {
+      props.history.push('/login')
+    } else {
+      const { url, description } = values
+      const newLink = {
+        url,
+        description,
+        postedBy: {
+          id: user.uid,
+          name: user.displayName
+        },
+        votes: [],
+        comments: [],
+        created: Date.now()
+      }
+
+      firebase.db.collection('links').add(newLink)
+      props.history.push('/')
+    }
   }
 
   return (
