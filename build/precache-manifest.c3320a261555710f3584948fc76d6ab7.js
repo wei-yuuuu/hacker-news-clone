6e02083b71f6b3e6ed20a766e84cec321b579b87
takeu <tony40508@gmail.com>
2019-08-25 02:16:29 +0800

    deploy: build

---
+++ build/precache-manifest.c3320a261555710f3584948fc76d6ab7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ac1595d17cc8bebfb8fc",
+    "url": "/static/js/main.23b51d4e.chunk.js"
+  },
+  {
+    "revision": "d2252c1c0ce8120bd087",
+    "url": "/static/js/2.17a39bb3.chunk.js"
+  },
+  {
+    "revision": "ac1595d17cc8bebfb8fc",
+    "url": "/static/css/main.81cbdf54.chunk.css"
+  },
+  {
+    "revision": "eb1eb4f1e9ee5d0ce5f9aa6fabaac9aa",
+    "url": "/index.html"
+  }
+];
